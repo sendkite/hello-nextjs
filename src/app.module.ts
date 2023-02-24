68282549aab43b680db1c4fa6a492ab6e525b112
sendkite <sendkite1@gmail.com>
2023-02-24 11:01:29 +0900

    added config module handling env variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,18 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { BookmarksModule } from './bookmarks/bookmarks.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CatsModule, UsersModule, AuthModule, BookmarksModule, PrismaModule],
+  imports: [
+    CatsModule,
+    UsersModule,
+    AuthModule,
+    BookmarksModule,
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
 })
 export class AppModule {}
